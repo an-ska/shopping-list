f85d0ed2952f08695104122b24487322caa0eb64
an-ska <1.anna.piotrowska@gmail.com>
2020-09-12 17:10:59 +0200

    fix mongodb and heroku deploys

--- server/router.js
+++ server/router.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const messages = require('./messages.json')
 
-router.get("/", (req, res) => {
-	res.send({ response: messages.serverIsRunning }).status(200);
+router.get("*", (req, res) => {
+	res.send({ response: 'Server is upp and running' }).status(200)
 })
 
-module.exports = router;
+module.exports = router
