824f6253826027aa14660e0a938c209336a57004
an-ska <anna.maria.prokopiuk@gmail.com>
2020-11-10 20:21:11 +0100

    reafactoring, adding possibility to use different lists by various users

--- server/index.js
+++ server/index.js
@@ -8,11 +8,11 @@ const server = http.createServer(app)
 const io = socketio(server, { pingTimeout: 6000000, pingInterval: 30000 })
 const messages = require('./messages.json')
 const cors = require('cors')
-const dotenv = require("dotenv")
-const { testDB } = require("./constants")
+const dotenv = require('dotenv')
+const { v4: uuidv4 } = require('uuid')
 
 // use to run the server with PORT and mongoDB set in env file
-// dotenv.config()
+dotenv.config()
 
 const PORT = process.env.PORT || 5000
 const mongoDB = process.env.MONGODB_URI || testDB
@@ -20,99 +20,125 @@ const mongoDB = process.env.MONGODB_URI || testDB
 app.use(router)
 app.use(cors())
 
-
 mongo.connect(mongoDB, (error, client) => {
 	if (error) throw error
 	console.log(messages.dbConnected)
 
-	let usersNumber = 0;
-	const getMessage = usersNumber => (usersNumber === 1 ? `${usersNumber} person connected!` : `${usersNumber} people are connected!`)
+	let usersNumber = 0
+	const getMessage = usersNumber => {
+		if (usersNumber > 1) {
+			return `${usersNumber} people are connected!`
+		}
+		if (usersNumber === 1) {
+			`${usersNumber} person connected!`
+		}
+
+		return
+	} 
 
 	io.on('connect', (socket) => {
 		let db = client.db('shoppingListAppDB')
-		let shoppingList = db.collection('shoppingList')
-		let boughtProductsList = db.collection('boughtProductsList')
+		// let userCollection = db.collection('1')
+		let userCollection
+		let listId
 
-		shoppingList
+		// userCollection
+		// .find()
+		// .sort({_id: 1})
+		// .toArray((error, response) => {
+		// 	if (error) throw error
+		// 	socket.emit('products', response)
+		// })
+
+		socket.on('join', (id) => {
+			listId = id
+			userCollection = db.collection(listId)
+
+			userCollection
 			.find()
 			.sort({_id: 1})
 			.toArray((error, response) => {
 				if (error) throw error
-				socket.emit('shoppingList', response)
-			})
-		boughtProductsList
-			.find()
-			.sort({_id: 1})
-			.toArray((error, response) => {
-				if (error) throw error
-				socket.emit('boughtProductsList', response)
+				socket.emit('products', response)
 			})
 
-		socket.on('join', () => {
 			usersNumber++
 			console.log(messages.newJoiner)
 
-			socket.emit('message',{ text: messages.welcome});
-			socket.broadcast.emit('message',{ text: getMessage(usersNumber) })
+			socket.join(listId)
+
+			socket.emit('message', { text: messages.welcome});
+			socket.broadcast.to(listId).emit('message', { text: getMessage(usersNumber) })
 		})
 
 		socket.on('addProduct', (name, callback) => {
-			shoppingList.insertOne({name, isChecked: false, id: Math.random()}, () => {
-				io.emit('addedProduct', {name, isChecked: false, id: Math.random()} )
+			const id = uuidv4()
+			userCollection.insertOne({name, isBought: false, isChecked: false, id}, () => {
+				io.to(listId).emit('addedProduct', {name, isBought: false, isChecked: false, id} )
 			})
 
 			callback()
 		})
 
 		socket.on('markProductAsBought', name => {
-			shoppingList.deleteMany({name}, (error, isProductDeleted) => {
-				if (error) throw error
-				if (isProductDeleted) console.log(messages.productDeleted)
-
-				io.emit('movedProduct', name)
-			})
-
-			boughtProductsList.insertOne({name}, () => {
-				io.emit('addedBoughtProduct', {name} )
-			})
+			userCollection.updateMany(
+				{name},
+				{$set: {isBought: true}},
+				(error, isProductBought) => {
+					if (error) throw error
+					if (isProductBought) {
+						console.log(messages.isBought)
+						io.to(listId).emit('markedProductAsBought', name)
+						socket.emit('message', { text: getMessage(messages.isBought) })
+					}
+				}
+			)
 		})
 
 		socket.on('clearShoppingList', () => {
-			shoppingList.drop((error, isListDeleted) => {
+			userCollection.deleteMany(
+				{isBought: false},
+				(error, isListDeleted) => {
 				if (error) throw error
-				if (isListDeleted) console.log(messages.shoppingListDeleted)
-
-				io.emit('clearedShoppingList')
+				if (isListDeleted) {
+					console.log(messages.shoppingListDeleted)
+					io.to(listId).emit('clearedShoppingList')
+				}
 			})
 		})
 
 		socket.on('clearBoughtProductsList', () => {
-			boughtProductsList.drop((error, isListDeleted) => {
+			userCollection.deleteMany(
+				{isBought: true},
+				(error, isListDeleted) => {
 				if (error) throw error
-				if (isListDeleted) console.log(messages.boughtProductsListDeleted)
-
-				io.emit('clearedBoughtProductsList')
+				if (isListDeleted) {
+					console.log(messages.boughtProductsListDeleted)
+					io.to(listId).emit('clearedBoughtProductsList')
+				}
 			})
 		})
 
 		socket.on('updateProduct', (oldName, newName, id) => {
-			shoppingList.updateOne(
+			userCollection.updateOne(
 				{name: oldName},
 				{$set: {name: newName}},
 				(error, isProductUpdated) => {
 					if (error) throw error
-					if (isProductUpdated) console.log(messages.productUpdated)
-
-					io.emit('editedProduct', newName, id)
+					if (isProductUpdated) {
+						console.log(messages.productUpdated)
+						io.to(listId).emit('editedProduct', newName, id)
+					}
 			})
 		})
 
 		socket.on('deleteProduct', id => {
-			shoppingList.deleteOne({id},(error, isProductDeleted) => {
+			userCollection.deleteOne({id}, (error, isProductDeleted) => {
 				if (error) throw error
-				if (isProductDeleted) console.log(messages.productDeleted)
-
-				io.emit('deletedProduct', id)
+				if (isProductDeleted) {
+					console.log(messages.productDeleted)
+					io.to(listId).emit('deletedProduct', id)
+				}
 			})
 		})
 
@@ -122,7 +148,7 @@ mongo.connect(mongoDB, (error, client) => {
 			usersNumber--
 			console.log(messages.userLeft)
 
-			socket.broadcast.emit('message',{ text: getMessage(usersNumber) })
+			socket.broadcast.to(listId).emit('message', { text: getMessage(usersNumber) })
 		})
 	})
 })
