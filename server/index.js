881043216394283c3965a811608a6e3d8b10bcf8
an-ska <anna.maria.prokopiuk@gmail.com>
2020-11-16 19:24:55 +0100

    change import order

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
 const mongo = require('mongodb').MongoClient
-const router = require('./router')
-const messages = require('./messages.json')
-const express = require("express")
+const express = require('express')
 const app = express()
 const socketio = require('socket.io')
-const http = require("http")
+const http = require('http')
+const router = require('./router')
 const server = http.createServer(app)
 const io = socketio(server, { pingTimeout: 6000000, pingInterval: 30000 })
 const cors = require('cors')
 const dotenv = require('dotenv')
-const initSocketConnection = require("./socketConnection")
+const initSocketConnection = require('./socketConnection')
+const messages = require('./messages.json')
 
 // use to run the server with PORT & mongoDB consts set in .env file as config reads the .env file so process.env has the keys & values defined in there
 // dotenv.config()
