9f5b7919b22aa288491f28fa76926c47829b2d80
an-ska <anna.maria.prokopiuk@gmail.com>
2020-11-16 19:09:42 +0100

    router fix

--- server/router.js
+++ server/router.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const messages = require('./messages.json')
 
-module.exports = router.get("/", (req, res) => {
+router.get("/", (req, res) => {
 	res.send({ response: messages.serverIsRunning }).status(200)
 })
+module.exports = router
